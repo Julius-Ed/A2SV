251c0413c5784b01ffa7348c2a30c5dd67849dea
Julius von Davier <vdavier@protonmail.com>
2022-01-21 22:34:59 +0100

    added merge sort implementation

--- Largest_Number.py
+++ Largest_Number.py
@@ -1,5 +1,4 @@
 
-
 class Solution:
     def largestNumber(self, nums):
         
@@ -24,7 +23,71 @@ class Solution:
             return "0"
 
         return result
+    
+    def compare(self, a, b):
+
+        option_a = str(a) + str(b)
+        option_b = str(b) + str(a)
+
+        if int(option_a) >= int(option_b):
+            return True
+        else:
+            return False
+
+
+    def merge(self, left, right):
+        result = []
+
+        while left and right:
+            #if left[0] >= right[0]:
+            if self.compare(left[0], right[0]):
+                result.append(left.pop(0))
+            else:
+                result.append(right.pop(0))
+        
+        if left:
+            result += left
+        if right:
+            result += right
+        
+        return result
+
+
+    def merge_sort(self, list):
+
+        # def base case
+        if len(list) <= 1:
+            return list
+        
+
+        middle = len(list) // 2
+
+        left = list[:middle]
+        right = list[middle:]
+
+        # recursive calls
+
+        left_sorted = self.merge_sort(left)
+        right_sorted = self.merge_sort(right)
+
+
+        return self.merge(left_sorted, right_sorted)
+    
+    def largestNumber2(self, nums):
+
+        results = self.merge_sort(nums)
+
+        result_string = ""
+
+        for result in results:
+            result_string += str(result)
+        
+        return result_string
+
 
 
 Sol = Solution()
-print(Sol.largestNumber([3,30,34,5,9]))
+#print(Sol.largestNumber([3,30,34,5,9]))
+
+
+print(Sol.largestNumber2([3, 30 , 34, 5 , 9]))
