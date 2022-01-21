b5e323ca664633c070bcffd957e6f24353665ac2
Julius von Davier <vdavier@protonmail.com>
2022-01-21 18:49:33 +0100

    improved formatting

--- Find_the_Kth_Largest_Integer_in_the_Array.py
+++ Find_the_Kth_Largest_Integer_in_the_Array.py
@@ -23,7 +23,7 @@ class Solution:
 
         return str(min_heap[0])
 
-    def quickSelect(self, nums, k):
+    def kthLargestNumber3(self, nums, k):
 
         for i in range(len(nums)):
             nums[i] = int(nums[i])
@@ -41,17 +41,15 @@ class Solution:
         pivot_index = left
         pivot = nums[pivot_index]
 
-        while True:  
+        while True:
 
             if not first_round:
                 pivot_index = left
                 pivot = nums[pivot_index]
-            
+
             first_round = False
 
             while left < right:
-
-
 
                 while left < len(nums) and nums[left] <= pivot:
                     left += 1
@@ -62,11 +60,7 @@ class Solution:
                 if left < right:
                     nums[left], nums[right] = nums[right], nums[left]
 
-                
-
             nums[right], nums[pivot_index] = nums[pivot_index], nums[right]
-
-
 
             if right == target_index:
                 return nums[right]
@@ -81,22 +75,18 @@ class Solution:
                 left = 0
                 right = target_index
 
-        return nums[right]
-
 
 Sol = Solution()
-# print(Sol.kthLargestNumber(["3", "6", "7", "10"], k=4))
-# print(Sol.kthLargestNumber(["2", "21", "12", "1"], k=3))
-# print(Sol.kthLargestNumber(["0", "0"], k=2))
 
-# print(Sol.kthLargestNumber2(["3", "6", "7", "10"], k=4))
-#print(Sol.kthLargestNumber2(["2", "21", "12", "1"], k=3))
-# print(Sol.kthLargestNumber2(["0", "0"], k=2))
+print(Sol.kthLargestNumber(["3", "6", "7", "10"], k=4))
+print(Sol.kthLargestNumber(["2", "21", "12", "1"], k=3))
+print(Sol.kthLargestNumber(["0", "0"], k=2))
 
-
-# print(Sol.quickSelect([3, 6, 7, 10], k=4))
-# print(Sol.quickSelect([2, 21, 12, 1], k=3))
-# print(Sol.quickSelect(["0", "0"], k=2))
+print(Sol.kthLargestNumber2(["3", "6", "7", "10"], k=4))
+print(Sol.kthLargestNumber2(["2", "21", "12", "1"], k=3))
+print(Sol.kthLargestNumber2(["0", "0"], k=2))
 
 
-print(Sol.quickSelect(["2", "21", "12", "1", "10", "0"], 3))
+print(Sol.kthLargestNumber3(["3", "6", "7", "10"], k=4))
+print(Sol.kthLargestNumber3(["2", "21", "12", "1"], k=3))
+print(Sol.kthLargestNumber3(["0", "0"], k=2))
