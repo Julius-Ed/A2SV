85f3aca55e579b020b6986aa080c51ed80da1197
Julius von Davier <vdavier@protonmail.com>
2022-01-28 13:12:18 +0100

    adjusted formatting and cleaned code

--- Design_Linked_List.py
+++ Design_Linked_List.py
@@ -2,7 +2,6 @@
 class ListNode:
 
     def __init__(self, val=None, next=None) -> None:
-
         self.val = val
         self.next = next
 
@@ -10,13 +9,10 @@ class ListNode:
 class MyLinkedList:
 
     def __init__(self):
-
         self.head = None
 
     # Get the value of the indexth node in the linked list. If the index is invalid, return -1
-
     def get(self, index: int) -> int:
-
         curr = self.head
 
         i = 0
@@ -30,15 +26,12 @@ class MyLinkedList:
             return -1
 
     # Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-
     def addAtHead(self, val: int) -> None:
-
         new_node = ListNode(val, self.head)
         self.head = new_node
 
     # Append a node of value val as the last element of the linked list.
     def addAtTail(self, val: int) -> None:
-
         curr = self.head
 
         if not curr:
@@ -51,11 +44,9 @@ class MyLinkedList:
         curr.next = ListNode(val)
 
     def addAtIndex(self, index: int, val: int) -> None:
-
         if index == 0:
             self.addAtHead(val)
 
-
         prev = None
         curr = self.head
 
@@ -64,24 +55,20 @@ class MyLinkedList:
             prev = curr
             curr = curr.next
             i += 1
-        
+
         if curr and prev:
             prev.next = ListNode(val, curr)
-        
-        # if not prev and curr:
-        #     self.addAtHead(val)
-        
+
         if not curr and prev:
             self.addAtTail(val)
 
-
     # Delete the indexth node in the linked list, if the index is valid.
-    def deleteAtIndex(self, index: int) -> None:
 
+    def deleteAtIndex(self, index: int) -> None:
         if self.head and not self.head.next and index == 0:
             self.head = None
             return
-        
+
         prev = None
         curr = self.head
 
@@ -90,36 +77,24 @@ class MyLinkedList:
             prev = curr
             curr = curr.next
             i += 1
-        
+
         if curr and (prev and curr.next):
-            #curr.next = None
             prev.next = curr.next
-        
+
         if curr and (not prev and curr.next):
             self.head = self.head.next
-        
+
         if curr and (prev and not curr.next):
             prev.next = None
 
-
+    # helper method to traverse and print the entire linkedlist.
     def traverse(self):
-
         curr = self.head
-        print("\n")
+
         while curr:
             print(curr.val)
             curr = curr.next
         print("\n")
-
-
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
-
 
 
 Sol = MyLinkedList()
