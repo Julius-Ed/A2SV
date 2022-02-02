fa819e22481fcb06d28e035043a2fbb977ffa294
Julius von Davier <vdavier@protonmail.com>
2022-02-02 17:48:52 +0100

    adjusted formatting

--- First_Missing_Positive.py
+++ First_Missing_Positive.py
@@ -1,11 +1,11 @@
 class Solution:
     def firstMissingPositive(self, nums) -> int:
-        
+
         for index, num in enumerate(nums):
-            
+
             if num < 0:
                 nums[index] = 0
-        
+
         for index, num in enumerate(nums):
 
             if abs(num) >= 1 and abs(num) <= len(nums) and nums[abs(num) - 1] == 0:
@@ -14,14 +14,12 @@ class Solution:
 
             if abs(num) >= 1 and abs(num) <= len(nums) and nums[abs(num) - 1] > 0:
                 nums[abs(num) - 1] = nums[abs(num) - 1] * (-1)
-            
-        
+
         for candidate_solution in range(0, len(nums)):
             if nums[candidate_solution] >= 0:
                 return candidate_solution + 1
-        
+
         return len(nums) + 1
-        
 
 
 Sol = Solution()
