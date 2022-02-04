58710cab7480712fea39995a1f22ce2f6df5627c
Julius von Davier <vdavier@protonmail.com>
2022-02-04 20:59:48 +0100

    adjusted formatting

--- Car_Fleet.py
+++ Car_Fleet.py
@@ -1,18 +1,17 @@
 from typing import List
 
+
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        
+
         positionsAndSpeed = []
         fleetCounter = 0
 
         for carPosition, carSpeed in zip(position, speed):
             positionsAndSpeed.append((carPosition, carSpeed))
-        
+
         positionsAndSpeed.sort()
         carStack = positionsAndSpeed
-
-
 
         while carStack:
             while len(carStack) > 1:
@@ -40,13 +39,10 @@ class Solution:
         return fleetCounter
 
 
-
-
 Sol = Solution()
 
-print(Sol.carFleet(12, [10,8,0,5,3], [2,4,1,1,3]) == 3)
+print(Sol.carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]) == 3)
 print(Sol.carFleet(10, [3], [3]) == 1)
 print(Sol.carFleet(100, [0, 2, 4], [4, 2, 1]) == 1)
 print(Sol.carFleet(10, [6, 8], [3, 2]) == 2)
 print(Sol.carFleet(10, [0, 4, 2], [2, 1, 3]) == 1)
-
