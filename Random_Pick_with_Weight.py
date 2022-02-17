86327f069fd52ed61db2cc9a7f6fa65d7a82fb3e
Julius von Davier <vdavier@protonmail.com>
2022-02-17 14:23:19 +0100

    added random pick problem

---
+++ Random_Pick_with_Weight.py
@@ -0,0 +1,25 @@
+from typing import List
+from random import choices
+
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.distribution = []
+        self.indices = [i for i in range(len(w))]
+
+        totalWeight = sum(w)
+        for weight in w:
+            self.distribution.append(weight/totalWeight)
+
+        
+
+    def pickIndex(self) -> int:
+        
+        return choices(self.indices, self.distribution, k=1)[0]
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
