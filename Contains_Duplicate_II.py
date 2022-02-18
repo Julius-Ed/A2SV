5a5800cc8ca5dd569d98a755a2a7ab5436414acc
Julius von Davier <vdavier@protonmail.com>
2022-02-18 13:25:27 +0100

    added average problem

--- Contains_Duplicate_II.py
+++ Contains_Duplicate_II.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        
+
         valuesInWindow = set()
         valuesInWindow.add(nums[0])
 
@@ -14,23 +14,21 @@ class Solution:
             if right - left >= k:
                 valuesInWindow.remove(nums[left])
                 left += 1
-            
+
             else:
 
                 if nums[right + 1] in valuesInWindow:
                     return True
                 else:
                     valuesInWindow.add(nums[right + 1])
-        
-                right += 1
-        
-        return False
 
+                right += 1
+
+        return False
 
 
 Sol = Solution()
 
 print(Sol.containsNearbyDuplicate([1, 2, 3, 1], 3) == True)
 print(Sol.containsNearbyDuplicate([1, 0, 1, 1], 1) == True)
-print(Sol.containsNearbyDuplicate([1,2,3,1,2,3], 2) == False)
-
+print(Sol.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2) == False)
---
+++ Maximum_Average_Subarray_I.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        
+
+        left, right = 0, k - 1
+        totalWindow = sum(nums[:right  + 1])
+        result = totalWindow / k
+
+        while right < len(nums) - 1:
+
+            totalWindow -= nums[left]
+
+            left += 1
+            right += 1
+
+            totalWindow += nums[right]
+
+            result = max(result, totalWindow / k)
+    
+        return result
+
+
+
+
+Sol = Solution()
+print(Sol.findMaxAverage([1,12,-5,-6,50,3], 4))
+print(Sol.findMaxAverage([5], 1))
