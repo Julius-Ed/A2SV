5334e18253be98133ff8b6a1b458f55d9b1498cc
Julius von Davier <vdavier@protonmail.com>
2022-02-21 14:55:43 +0100

    added replace smallest possible substring problem

--- Replace_the_Substring_for_Balanced_String.py
+++ Replace_the_Substring_for_Balanced_String.py
@@ -1,59 +1,60 @@
-from collections import Counter
-
 
 class Solution:
     def balancedString(self, s: str) -> int:
-        occ = Counter(s)
-        req = {}
-        res = len(s)
 
-        for char in occ:
-            if occ[char] > len(s) // 4:
-                req[char] = occ[char] - len(s) // 4
+        charMap = [0] * 4
+        targetWindow = [0] * 4
+        currentWindow = [0] * 4
+        res = float("inf")
 
-        if len(req) == 0:
+        for c in s:
+            charMap[self.charToInt(c)] += 1
+
+        for index, val in enumerate(charMap):
+            if val - len(s) // 4 > 0:
+                targetWindow[index] = val - (len(s) // 4)
+
+        if targetWindow == [0, 0, 0, 0]:
             return 0
 
-        window = {}
-        left = 0
+        left, right = -1, 0
 
-        for right in range(len(s)):
-            if s[right] not in window:
-                window[s[right]] = 1
-            else:
-                window[s[right]] += 1
+        while right < len(s):
+            
+            currentWindow[self.charToInt(s[right])] += 1
 
-            if self.checkBalance(req, window):
-                res = min(res, right - left + 1)
-
-                while left < right:
-
-                    if window[s[left]] <= 1:
-                        window.pop(s[left])
-                    else:
-                        window[s[left]] -= 1
-
-                    left += 1
-
-                    if self.checkBalance(req, window):
-                        res = min(res, right - left + 1)
-
+            while self.validateWindow(targetWindow, currentWindow) and left < right:
+                res = min(res, right - left)
+                left += 1
+                currentWindow[self.charToInt(s[left])] -= 1
+            
+            right += 1
+        
         return res
 
-    def checkBalance(self, req, window) -> bool:
+    def validateWindow(self, targetWindow, currentWindow):
 
-        for char in req:
-            if not (char in window and window[char] >= req[char]):
+        for valTarget, valCurrent in zip(targetWindow, currentWindow):
+            if valCurrent < valTarget:
                 return False
 
         return True
 
+    def charToInt(self, c):
+        if c == "Q":
+            return 0
+        if c == "W":
+            return 1
+        if c == "E":
+            return 2
+        if c == "R":
+            return 3
 
 Sol = Solution()
-print(Sol.balancedString("WQWRQQQW") == 3)
-print(Sol.balancedString("QWER") == 0)
-print(Sol.balancedString("QQWE") == 1)
-print(Sol.balancedString("QQQW") == 2)
+#print(Sol.balancedString("WQWRQQQW") == 3)
+#print(Sol.balancedString("QWER") == 0)
+#print(Sol.balancedString("QQWE") == 1)
+#print(Sol.balancedString("QQQW") == 2)
 
 
 print(Sol.balancedString("WQWRQEQQWERQWWWEREWRQQWWWWQW") == 6)
