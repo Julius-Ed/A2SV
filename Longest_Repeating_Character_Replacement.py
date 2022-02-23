2174ec5d35570e79ca6707e2b0951653bda5bf93
Julius von Davier <vdavier@protonmail.com>
2022-02-23 17:28:55 +0100

    adjusted design

--- Longest_Repeating_Character_Replacement.py
+++ Longest_Repeating_Character_Replacement.py
@@ -1,15 +1,9 @@
 
-
-from turtle import right
-
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
-        
+
         count = {}
         res = 0
-
         left = 0
 
         for right in range(len(s)):
@@ -21,13 +15,7 @@ class Solution:
                 left += 1
 
             res = max(res, right - left + 1)
-    
         return res
 
 
-
 Sol = Solution()
-print(Sol.characterReplacement("ABAB", 2) == 4)
-print(Sol.characterReplacement("AABABBA", 1))
-# print(Sol.characterReplacement("ABAA", 0))
-# print(Sol.characterReplacement("ABBB", 2))
