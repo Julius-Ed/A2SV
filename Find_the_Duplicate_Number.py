cf01bdfbf29fe97d445c98f1d3f2849ef89ecaa1
Julius von Davier <vdavier@protonmail.com>
2022-04-04 11:34:42 +0200

    added comments

--- Find_the_Duplicate_Number.py
+++ Find_the_Duplicate_Number.py
@@ -1,15 +1,20 @@
 from typing import List
 
+# O(n*log(n))
+
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
+        # the duplicate value must be in this range.
         lowTarget, highTarget = 0, len(nums) - 1
 
+        # O(log(n))
         while lowTarget <= highTarget:
 
             mid = (lowTarget + highTarget) // 2
 
+            # O(n)
             if self.duplicate(nums, mid):
                 dup = mid
                 highTarget = mid - 1
@@ -19,7 +24,7 @@ class Solution:
         return dup
 
     def duplicate(self, nums, target):
-
+        # the lowest number for which numbers with a face value lower or equal to target exist in nums, is a duplicate.
         count = 0
 
         for num in nums:
