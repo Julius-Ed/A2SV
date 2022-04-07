46fea42dbe0f705fce041d43fd03b44485053a50
Julius von Davier <vdavier@protonmail.com>
2022-04-07 11:32:12 +0200

    added comments and time complexity

--- Rotting_Oranges.py
+++ Rotting_Oranges.py
@@ -24,6 +24,7 @@ class Solution:
                 if grid[row][col] == 2:
                     rottenStart.append((row, col, 0))
 
+        # start with all rotten oranges in the queue.
         q = deque(rottenStart)
 
         fourDimensions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
@@ -37,12 +38,16 @@ class Solution:
             for dimension in fourDimensions:
                 x, y = dimension
 
+                # update the level (minutes) by for each iteration.
                 candidate = (row + y, col + x, mins + 1)
 
+                # check for out of bound dimensions. Only consider oranges that have not been rotten yet.
                 if 0 <= candidate[0] < rows and 0 <= candidate[1] < cols and grid[candidate[0]][candidate[1]] == 1:
+                    # mark orange as rotten.
                     grid[candidate[0]][candidate[1]] = 2
                     q.append(candidate)
 
+        # check if any oranges have not been reached via bfs.
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == 1:
