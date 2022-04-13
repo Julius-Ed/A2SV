c4dbaf4116ddaf8e65f2db1c323bf7613a3c6e07
Julius von Davier <vdavier@protonmail.com>
2022-04-13 17:21:55 +0200

    added maxDepth problem

---
+++ Maximum_Depth_of_N-ary_Tree.py
@@ -0,0 +1,37 @@
+
+from typing import Node
+
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+
+        if not root:
+            return 0
+
+        self.res = 0
+
+        def dfs(curr, depth):
+
+            if not curr:
+                return
+
+            if depth > self.res:
+                self.res = depth
+
+            for child in curr.children:
+                dfs(child, depth + 1)
+
+        dfs(root, 1)
+
+        return self.res
+
+
+Sol = Solution()
+
+root = Node(1, [])
