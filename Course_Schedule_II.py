0a5c297014c53b288fd91c8606e21c1e083aac2e
Julius von Davier <vdavier@protonmail.com>
2022-04-22 15:35:05 +0200

    added course schedule ii

---
+++ Course_Schedule_II.py
@@ -0,0 +1,70 @@
+from audioop import reverse
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        adjacencyList = {}
+        inbound = {}
+
+        # create empty adjacency and inbound maps.
+        for i in range(numCourses):
+            inbound[i] = 0
+            adjacencyList[i] = []
+
+        # populate adjacency list
+        for a, b in prerequisites:
+            adjacencyList[a].append(b)
+
+        # count inbounds
+        for _, b in prerequisites:
+            inbound[b] += 1
+
+        q = deque([])
+        # start with those nodes that don't have inbounds
+        for course in inbound:
+            if inbound[course] == 0:
+                q.append(course)
+
+        res = []
+        while q:
+
+            currCourses = q.popleft()
+            res.append(currCourses)
+
+            # check whether a loop exists
+            if len(res) > numCourses:
+                return []
+
+            children = adjacencyList[currCourses]
+
+            # decrease inbounds for children since we removed a node.
+            for child in children:
+                inbound[child] -= 1
+
+                # nodes without inbounds can be appended to the que.
+                if inbound[child] == 0:
+                    q.append(child)
+
+        if len(res) != numCourses:
+            return []
+
+        res.reverse()
+        return res
+
+
+Sol = Solution()
+
+
+numCourses = 5
+prerequisites = [
+    [0, 1],
+    [0, 2],
+    [1, 3],
+    [1, 4],
+    [2, 4]
+]
+Sol = Solution()
+print(Sol.findOrder(numCourses, prerequisites))
